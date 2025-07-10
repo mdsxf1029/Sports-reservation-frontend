887322a87ef5748713419acdbfdefc7b06a41477
Zhang Xiang <2352985@tongji.edu.cn>
2025-07-10 16:25:40 +0800

    Update
    搭建了网站的简单框架，补充了一些全局配置

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <HeaderNavbar class="header-navbar"/>
+    <div class="route-view-container">
+
+    </div>
+    <FooterNavbar class="footer-navbar"/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+import HeaderNavbar from './components/HeaderNavbar.vue'
+import FooterNavbar from './components/FooterNavbar.vue'
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.header-navbar{
+  position: fixed;
+  z-index: 1000;
+  width: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.route-view-container{
+  display: flex;
+  flex-direction: column;
+  padding-top: 60px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.footer-navbar{
+  width: 100%;
 }
+
 </style>
---
+++ src/components/FooterNavbar.vue
@@ -0,0 +1,105 @@
+
+<template>
+  <el-container class="footer-navbar">
+    <a href="https://cs.tongji.edu.cn/" target="_blank">
+      <img src="../assets/LogosAndIcons/Tongji.png"
+           alt="TongjiLogo"
+           class="footer-logo"
+           height="40"/>
+    </a>
+
+    <a href="https://github.com/mdsxf1029/Sports-reservation-frontend" target="_blank">
+      <img src="../assets/LogosAndIcons/GitHub.png"
+           alt="GithubLogo"
+           class="footer-logo"
+           height="40"/>
+    </a>
+
+    <div class='footer-content'>
+      <p>
+        <el-link :underline='false' class='footer-link' >
+          关于我们
+        </el-link>
+        <span class="separator">|</span>
+        <el-link :underline='false' class='footer-link' @click='showPrivacyPolicy=true'>
+          隐私政策
+        </el-link>
+        <span class="separator">|</span>
+        <el-link :underline='false' class='footer-link' @click='showDisclaimer=true'>
+          免责声明
+        </el-link>
+        <span class="separator">|</span>
+        <el-link :underline='false' class='footer-link' @click='showCodeOfConduct=true'>
+          行为准则
+        </el-link>
+        <span class="separator">|</span>
+        <el-link :underline='false' class='footer-link' @click='showContactUs=true'>
+          联系我们
+        </el-link>
+        <span class="separator">|</span>
+        <el-link :underline='false' class='footer-link' @click='showFeedback=true'>
+          反馈
+        </el-link>
+      </p>
+    </div>
+  </el-container>
+
+  <el-dialog>
+
+  </el-dialog>
+
+  <el-dialog>
+
+  </el-dialog>
+
+  <el-dialog>
+
+  </el-dialog>
+
+  <el-dialog>
+
+  </el-dialog>
+
+  <el-dialog>
+
+  </el-dialog>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const showPrivacyPolicy= ref(false);
+const showDisclaimer = ref(false);
+const showCodeOfConduct = ref(false);
+const showContactUs = ref(false);
+const showFeedback = ref(false);
+
+</script>
+
+<style scoped>
+.footer-navbar{
+  position: relative;
+  width: 100%;
+  height: 70px;
+  justify-content: center;
+}
+
+.footer-logo{
+  margin-right: 10px;
+  margin-top: 10px;
+}
+
+.footer-content{
+  font-size:16px;
+}
+
+.separator {
+  color: #999;
+  user-select: none;
+  margin-left: 10px;
+  margin-right: 10px
+}
+
+.footer-link{
+  font-size:16px;
+}
+</style>
