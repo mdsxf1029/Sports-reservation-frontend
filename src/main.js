ae8b827e3806b605fca8ecfd93e9f90d94cf605d
冯俊财 <2353924@tongji.edu.cn>
2025-07-21 14:23:25 +0800

    添加中文语言包

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
 import router from './utils/router.js'
 
 const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.use(router)
 app.mount('#app')
