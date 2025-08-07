77eb21aefa8053f62e595844ef8aa2895e098f81
mdsxf1029 <145674548+mdsxf1029@users.noreply.github.com>
2025-08-08 01:26:20 +0800

    update：尾栏样式修改

--- src/components/FooterNavbar.vue
+++ src/components/FooterNavbar.vue
@@ -1,105 +1,93 @@
-
 <template>
-  <el-container class="footer-navbar">
-    <a href="https://cs.tongji.edu.cn/" target="_blank">
-      <img src="../assets/LogosAndIcons/Tongji.png"
-           alt="TongjiLogo"
-           class="footer-logo"
-           height="40"/>
-    </a>
+  <footer class="footer-navbar">
+    <div class="footer-center">
+      <a href="https://cs.tongji.edu.cn/" target="_blank">
+        <img
+          src="../assets/LogosAndIcons/Tongji.png"
+          alt="TongjiLogo"
+          class="footer-logo"
+          height="40"
+        />
+      </a>
+      <a href="https://github.com/mdsxf1029/Sports-reservation-frontend" target="_blank">
+        <img
+          src="../assets/LogosAndIcons/GitHub.png"
+          alt="GithubLogo"
+          class="footer-logo"
+          height="40"
+        />
+      </a>
 
-    <a href="https://github.com/mdsxf1029/Sports-reservation-frontend" target="_blank">
-      <img src="../assets/LogosAndIcons/GitHub.png"
-           alt="GithubLogo"
-           class="footer-logo"
-           height="40"/>
-    </a>
-
-    <div class='footer-content'>
-      <p>
-        <el-link :underline='false' class='footer-link' >
-          关于我们
-        </el-link>
+      <div class="footer-content">
+        <el-link :underline="false" class="footer-link">关于我们</el-link>
         <span class="separator">|</span>
-        <el-link :underline='false' class='footer-link' @click='showPrivacyPolicy=true'>
-          隐私政策
-        </el-link>
+        <el-link :underline="false" class="footer-link" @click="showPrivacyPolicy = true">隐私政策</el-link>
         <span class="separator">|</span>
-        <el-link :underline='false' class='footer-link' @click='showDisclaimer=true'>
-          免责声明
-        </el-link>
+        <el-link :underline="false" class="footer-link" @click="showDisclaimer = true">免责声明</el-link>
         <span class="separator">|</span>
-        <el-link :underline='false' class='footer-link' @click='showCodeOfConduct=true'>
-          行为准则
-        </el-link>
+        <el-link :underline="false" class="footer-link" @click="showCodeOfConduct = true">行为准则</el-link>
         <span class="separator">|</span>
-        <el-link :underline='false' class='footer-link' @click='showContactUs=true'>
-          联系我们
-        </el-link>
+        <el-link :underline="false" class="footer-link" @click="showContactUs = true">联系我们</el-link>
         <span class="separator">|</span>
-        <el-link :underline='false' class='footer-link' @click='showFeedback=true'>
-          反馈
-        </el-link>
-      </p>
+        <el-link :underline="false" class="footer-link" @click="showFeedback = true">反馈</el-link>
+      </div>
     </div>
-  </el-container>
-
-  <el-dialog>
-
-  </el-dialog>
-
-  <el-dialog>
-
-  </el-dialog>
-
-  <el-dialog>
-
-  </el-dialog>
-
-  <el-dialog>
-
-  </el-dialog>
-
-  <el-dialog>
-
-  </el-dialog>
+  </footer>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-const showPrivacyPolicy= ref(false);
-const showDisclaimer = ref(false);
-const showCodeOfConduct = ref(false);
-const showContactUs = ref(false);
-const showFeedback = ref(false);
+import { ref } from 'vue'
 
+const showPrivacyPolicy = ref(false)
+const showDisclaimer = ref(false)
+const showCodeOfConduct = ref(false)
+const showContactUs = ref(false)
+const showFeedback = ref(false)
 </script>
 
 <style scoped>
 .footer-navbar {
-  position: relative;
   width: 100%;
-  height: 70px;
+  padding: 16px 0;
+  background-color: #f8f8f8;
+  border-top: 1px solid #ddd;
+  display: flex;
   justify-content: center;
+  align-items: center;
+}
+
+.footer-center {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  max-width: 2000px; /* 限制宽度避免挤压换行 */
 }
 
 .footer-logo {
-  margin-right: 10px;
-  margin-top: 10px;
+  vertical-align: middle; /* 改为垂直对齐中间 */
 }
 
 .footer-content {
-  font-size:16px;
+  display: flex;
+  flex-wrap: nowrap; /* 避免多余换行 */
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  flex-shrink: 1;
+  /*overflow: hidden;*/
 }
 
 .separator {
   color: #999;
   user-select: none;
-  margin-left: 10px;
-  margin-right: 10px
+  margin: 0 10px;
 }
 
 .footer-link {
-  font-size:16px;
+  font-size: 16px;
+  white-space: nowrap; /* 保持一行 */
 }
+
 </style>
